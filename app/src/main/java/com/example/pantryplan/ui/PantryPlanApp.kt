f85665e126eaa5d5cf7ee821a4761f4b8cb003d2
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2025-04-25 12:17:10 +0100

    Add the CenterAlignedTopAppBar.

--- app/src/main/java/com/example/pantryplan/ui/PantryPlanApp.kt
+++ app/src/main/java/com/example/pantryplan/ui/PantryPlanApp.kt
@@ -2,30 +2,34 @@ package com.example.pantryplan.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.pantryplan.feature.pantry.navigation.PantryRoute
 import com.example.pantryplan.feature.pantry.navigation.pantryScreen
 import com.example.pantryplan.feature.recipes.navigation.recipesScreen
 import com.example.pantryplan.navigation.TopLevelDestination
 import com.example.pantryplan.ui.theme.PantryPlanTheme
-import androidx.compose.runtime.getValue
 import com.example.pantryplan.feature.meals.navigation.mealPlannerScreen
 
 @Composable
+@OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalComposeUiApi::class,
+)
 fun PantryPlanApp(appState: PantryPlanAppState) {
-    val navBackStackEntry by appState.navController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination
+    val currentDestination = appState.currentDestination
 
     NavigationSuiteScaffold(
         navigationSuiteItems = {
@@ -46,7 +50,19 @@ fun PantryPlanApp(appState: PantryPlanAppState) {
             }
         }
     ) {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                val destination = appState.currentTopLevelDestination
+                if (destination != null) {
+                    CenterAlignedTopAppBar(
+                        title = {
+                            Text(stringResource(destination.label))
+                        },
+                    )
+                }
+            },
+        ) { innerPadding ->
             NavHost(
                 navController = appState.navController,
                 startDestination = PantryRoute,
