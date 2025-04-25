094d40d02511af4b7436d9bf3f98e60257d5379d
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2025-04-25 12:17:10 +0100

    Add the CenterAlignedTopAppBar.

--- app/src/main/java/com/example/pantryplan/ui/PantryPlanAppState.kt
+++ app/src/main/java/com/example/pantryplan/ui/PantryPlanAppState.kt
@@ -4,7 +4,11 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.NavDestination.Companion.hasRoute
 import com.example.pantryplan.feature.meals.navigation.navigateToMealPlanner
 import com.example.pantryplan.feature.pantry.navigation.navigateToPantry
 import com.example.pantryplan.feature.recipes.navigation.navigateToRecipes
@@ -30,4 +34,20 @@ class PantryPlanAppState (
             TopLevelDestination.MEAL_PLANNER -> navController.navigateToMealPlanner()
         }
     }
+
+    val currentDestination: NavDestination?
+        @Composable
+        get() {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            navBackStackEntry?.let { return it.destination }
+            return navController.previousBackStackEntry?.destination
+        }
+
+    val currentTopLevelDestination: TopLevelDestination?
+        @Composable
+        get() {
+            return TopLevelDestination.entries.firstOrNull { topLevelDestination ->
+                navController.currentDestination?.hasRoute(topLevelDestination.route) == true
+            }
+        }
 }
