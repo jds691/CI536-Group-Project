ca5fe898a573e490aff23086019c6d04ec827fc7
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2025-04-29 15:26:51 +0100

    Add Application class. Separate hilt dependencies and plugins with a comment.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // Hilt plugins.
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
 }
@@ -65,8 +67,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.hilt.android)
     implementation(libs.kotlinx.serialization.json)
 
+    // Hilt dependencies.
+    implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
 }
