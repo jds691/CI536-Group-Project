738390b6baf66252f61f613e1f27eeff7ce81558
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-05-02 14:19:02 +0100

    Remove left over Greeting and GreetingPreview composables

--- app/src/main/java/com/example/pantryplan/ui/PantryPlanApp.kt
+++ app/src/main/java/com/example/pantryplan/ui/PantryPlanApp.kt
@@ -23,7 +23,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
@@ -114,21 +113,5 @@ fun PantryPlanApp(appState: PantryPlanAppState) {
             recipesScreen()
             mealPlannerScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    com.example.pantryplan.core.designsystem.theme.PantryPlanTheme {
-        Greeting("Android")
     }
 }
