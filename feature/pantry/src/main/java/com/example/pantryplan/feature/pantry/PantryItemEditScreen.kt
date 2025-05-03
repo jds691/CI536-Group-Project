3ec550cc2ae46d9620e68cba78dead6fbc48f628
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2025-05-03 20:45:35 +0100

    Add top bar to the pantry item edit screen.

--- feature/pantry/src/main/java/com/example/pantryplan/feature/pantry/PantryItemEditScreen.kt
+++ feature/pantry/src/main/java/com/example/pantryplan/feature/pantry/PantryItemEditScreen.kt
@@ -1,9 +1,19 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.pantryplan.feature.pantry
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import java.util.UUID
@@ -13,16 +23,32 @@ fun PantryItemEditScreen(
     existingId: UUID? = null,
     onBackClick: () -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Text(
-            text = "Hello Pantry item edit!"
-        )
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text("Add Item") },
+                navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(Icons.Default.Clear, "")
+                    }
+                },
+                actions = {
+                    TextButton(
+                        onClick = { /* TODO: Actually save */ }
+                    ) {
+                        Text("Save")
+                    }
+                }
+            )
+        }) { contentPadding ->
+        Column (modifier = Modifier.padding(contentPadding)) {
+            Text(
+                text = "Hello Pantry item edit!"
+            )
 
-        Button(onClick = onBackClick) {
-            Text("Back")
+            Button(onClick = onBackClick) {
+                Text("Back")
+            }
         }
     }
 }
