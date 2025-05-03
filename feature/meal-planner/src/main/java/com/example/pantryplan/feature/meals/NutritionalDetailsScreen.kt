66461fe3eb2f9a2e0813a612aa353ac35c4b7b65
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-05-03 23:12:23 +0100

    Initial pass of NutritionalDetailsScreen TODO: - Fix colours on MacrosCard - Find a way to shorten Nutrients section - Fix formatting of NutrientRow

--- feature/meal-planner/src/main/java/com/example/pantryplan/feature/meals/NutritionalDetailsScreen.kt
+++ feature/meal-planner/src/main/java/com/example/pantryplan/feature/meals/NutritionalDetailsScreen.kt
@@ -2,25 +2,40 @@
 
 package com.example.pantryplan.feature.meals
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pantryplan.core.designsystem.R
 import com.example.pantryplan.core.designsystem.theme.PantryPlanTheme
+import com.example.pantryplan.core.models.NutritionInfo
+import com.example.pantryplan.feature.meals.ui.MacrosCard
 
 @Composable
 fun NutritionalDetailsScreen(
-    onBackClick: () -> Unit
+    onBackClick: () -> Unit,
+
+    modifier: Modifier = Modifier
 ) {
     Scaffold(
         topBar = {
@@ -35,20 +50,187 @@ fun NutritionalDetailsScreen(
         }
     ) { innerPadding ->
         Column(
-            modifier = Modifier
-                .consumeWindowInsets(innerPadding)
+            verticalArrangement = Arrangement
+                .spacedBy(8.dp),
+            modifier = modifier
                 .padding(innerPadding)
+                .padding(horizontal = dimensionResource(R.dimen.horizontal_margin))
+                .verticalScroll(rememberScrollState())
         ) {
-            Text(
-                text = "Hello Nutritional Details!"
-            )
+            Macros()
+
+            Nutrients()
+
+            Tips()
         }
+    }
+}
+
+@Composable
+private fun Macros(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement
+            .spacedBy(4.dp),
+
+        modifier = modifier
+    ) {
+        Text(
+            text = "Macros",
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+
+        MacrosCard(
+            item = NutritionInfo(
+                calories = 500,
+                fats = 13.35f,
+                saturatedFats = 22f,
+                carbohydrates = 65f,
+                sugar = 12f,
+                fiber = 34f,
+                protein = 30f,
+                sodium = 12f
+            )
+        )
+    }
+}
+
+@Composable
+private fun NutrientRow(
+    name: String,
+    amount: Float,
+    target: Float,
+
+    modifier: Modifier = Modifier,
+    unit: String = "g"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 40.dp)
+    ) {
+        Text(
+            text = name,
+            style = MaterialTheme.typography.labelMedium
+        )
+
+        Text(
+            text = "${amount}/${target}${unit}",
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Composable
+private fun Nutrients(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement
+            .spacedBy(4.dp),
+
+        modifier = modifier
+    ) {
+        Text(
+            text = "Nutrients",
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+
+        NutrientRow(
+            name = "Calories",
+            amount = 0f,
+            target = 2000f,
+            unit = "kcal"
+        )
+
+        HorizontalDivider()
+
+        NutrientRow(
+            name = "Fat",
+            amount = 0f,
+            target = 2000f,
+        )
+
+        HorizontalDivider()
+
+        NutrientRow(
+            name = "Saturated Fat",
+            amount = 0f,
+            target = 2000f,
+        )
+
+        HorizontalDivider()
+
+        NutrientRow(
+            name = "Carbohydrates",
+            amount = 0f,
+            target = 2000f,
+        )
+
+        HorizontalDivider()
+
+        NutrientRow(
+            name = "Sugar",
+            amount = 0f,
+            target = 2000f,
+        )
+
+        HorizontalDivider()
+
+        NutrientRow(
+            name = "Fiber",
+            amount = 0f,
+            target = 2000f,
+        )
+
+        HorizontalDivider()
+
+        NutrientRow(
+            name = "Protein",
+            amount = 0f,
+            target = 2000f,
+        )
+
+        HorizontalDivider()
+
+        NutrientRow(
+            name = "Sodium",
+            amount = 0f,
+            target = 2000f,
+        )
+    }
+}
+
+@Composable
+private fun Tips(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement
+            .spacedBy(4.dp),
+
+        modifier = modifier
+    ) {
+        Text(
+            text = "Tips",
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+
+        Text("No tips currently available.")
+
+        // TODO: Show a list of cards that can navigate to various tips
     }
 }
 
 @Preview
 @Composable
-internal fun NutritionalDetailsScreenPreview() {
+private fun NutritionalDetailsScreenPreview() {
     PantryPlanTheme {
         NutritionalDetailsScreen(
             onBackClick = {}
