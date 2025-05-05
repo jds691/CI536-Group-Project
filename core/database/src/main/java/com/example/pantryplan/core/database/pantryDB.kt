a6ac1463480c0d4e1caf2e0baf52bf19de80e571
Megan "Evil" Welter <em.welter@protonmail.com>
2025-05-05 17:59:58 +0100

    Added Room database for pantry entries with show all, add, delete and search

--- core/database/src/main/java/com/example/pantryplan/core/database/pantryDB.kt
+++ core/database/src/main/java/com/example/pantryplan/core/database/pantryDB.kt
@@ -1,12 +1,15 @@
 package com.example.pantryplan.core.database
 
+import androidx.room.Dao
 import androidx.room.Database
+import androidx.room.Delete
 import androidx.room.Entity
+import androidx.room.Insert
 import androidx.room.PrimaryKey
+import androidx.room.Query
 import androidx.room.RoomDatabase
 import java.sql.Date
 import java.util.UUID
-import com.example.pantryplan.core.data.access.PantryDAO // ???
 
 @Entity(tableName = "PantryStock")
 data class PantryStock(
@@ -19,9 +22,11 @@ data class PantryStock(
     val imageRefURL: String? // Can be null, TODO: Path
 )
 
+
+// Create Database with above class as fields
 @Database(entities = [PantryStock::class], version = 1)
-    abstract class PantryDB : RoomDatabase() {
-        abstract fun pantryDAO(): PantryDAO
+    abstract class PantryDB : RoomDatabase() { //Database class
+        abstract fun pantryDAO(): PantryDAO.PantryDAO // Hook data entity up to interface
     }
 
 /*
@@ -29,3 +34,34 @@ data class PantryStock(
     check date opened and compare to date expiring,
     then return time until expiry
 */
+
+
+//This is where most of the problems will be. I think it works, though.
+@Dao
+abstract class PantryDAO {
+    interface PantryDAO {
+        @Query("SELECT * FROM PantryStock")
+        fun showAll(): List<String>
+
+        @Query("SELECT * FROM PantryStock WHERE itemName LIKE :searchQuery")
+        fun searchByName(searchQuery: String): PantryStock
+
+        @Query("SELECT * FROM pantrystock")
+        fun calcDaysUntilExpired(): List<String>
+        //TODO; check date opened and compare to date,
+        // expiring then return time until expiry
+
+        @Insert
+        fun addItem(
+            itemName: String,
+            dateExpiring: java.util.Date,
+            dateOpened: java.util.Date,
+            quantity: Int,
+            itemState: Int
+            //TODO: Image reference URI
+        )
+
+        @Delete
+        fun removeItem(itemID: UUID); //Delete *row* containing specified item UUID
+    }
+}
