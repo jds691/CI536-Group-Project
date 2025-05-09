81537558e4307865f894f92dd48894290f515445
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-05-09 16:34:38 +0100

    Update dependencies to include remaining Room dependencies Also includes libraries for unit testing

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -27,18 +27,29 @@ android {
 }
 
 dependencies {
-
     api(libs.kotlinx.datetime)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.room.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Room
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
 
     // Hilt dependencies.
     implementation(libs.hilt.android)
     implementation(libs.androidx.hilt.navigation.compose)
     ksp(libs.hilt.compiler)
+
+    // Testing
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.androidx.room.testing)
+    androidTestImplementation(libs.kotlin.test)
+    androidTestImplementation(libs.androidx.test.core)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
 }
