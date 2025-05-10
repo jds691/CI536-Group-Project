92a6eab5b943bc6519d1ba11dc4ded1981bfa40c
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-05-10 15:18:29 +0100

    Fix KSP compilation errors kotlin.time.Duration is not a valid type to store by default

--- core/database/src/main/java/com/example/pantryplan/core/database/PantryPlanDatabase.kt
+++ core/database/src/main/java/com/example/pantryplan/core/database/PantryPlanDatabase.kt
@@ -3,6 +3,7 @@ package com.example.pantryplan.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.pantryplan.core.database.converter.DurationConverter
 import com.example.pantryplan.core.database.converter.InstantConverter
 import com.example.pantryplan.core.database.converter.UUIDConverter
 import com.example.pantryplan.core.database.dao.PantryDao
@@ -17,6 +18,7 @@ import com.example.pantryplan.core.database.model.PantryStock
 )
 @TypeConverters(
     InstantConverter::class,
+    DurationConverter::class,
     UUIDConverter::class
 )
 internal abstract class PantryPlanDatabase : RoomDatabase() { //Database class
