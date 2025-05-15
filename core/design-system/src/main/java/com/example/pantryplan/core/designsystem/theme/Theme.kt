f2b06f9d1cc5f962fa50f8a8caa5c5716180f34a
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-05-15 14:50:05 +0100

    Fix status bar colours

--- core/design-system/src/main/java/com/example/pantryplan/core/designsystem/theme/Theme.kt
+++ core/design-system/src/main/java/com/example/pantryplan/core/designsystem/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.pantryplan.core.designsystem.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -10,12 +9,8 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 @Immutable
 data class ExtendedColorScheme(
@@ -553,14 +548,6 @@ fun PantryPlanTheme(
 
         darkTheme -> darkScheme
         else -> lightScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
     }
 
     MaterialTheme(
