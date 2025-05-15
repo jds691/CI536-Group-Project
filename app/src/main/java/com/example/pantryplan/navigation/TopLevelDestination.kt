ff7eb5ac4d3d585256d084d375719899c70a691e
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-05-15 02:52:13 +0100

    Update app iconography

--- app/src/main/java/com/example/pantryplan/navigation/TopLevelDestination.kt
+++ app/src/main/java/com/example/pantryplan/navigation/TopLevelDestination.kt
@@ -1,18 +1,12 @@
 package com.example.pantryplan.navigation
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Place
-import androidx.compose.material.icons.outlined.BookmarkBorder
-import androidx.compose.material.icons.outlined.Notifications
-import androidx.compose.material.icons.outlined.Place
-import androidx.compose.material.icons.rounded.Bookmark
-import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.pantryplan.feature.meals.navigation.MealPlanner
 import com.example.pantryplan.feature.pantry.navigation.PantryList
 import com.example.pantryplan.feature.recipes.navigation.RecipesList
 import kotlin.reflect.KClass
+import com.example.pantryplan.core.designsystem.R as designSystemR
 import com.example.pantryplan.feature.meals.R as mealsR
 import com.example.pantryplan.feature.pantry.R as pantryR
 import com.example.pantryplan.feature.recipes.R as recipesR
@@ -20,29 +14,29 @@ import com.example.pantryplan.feature.recipes.R as recipesR
 enum class TopLevelDestination (
     @StringRes val iconTextId: Int,
     @StringRes val titleTextId: Int,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector,
+    @DrawableRes val selectedIconId: Int,
+    @DrawableRes val unselectedIconId: Int,
     val route: KClass<*>,
 ){
     PANTRY(
         pantryR.string.feature_pantry_title,
         pantryR.string.feature_pantry_title,
-        Icons.Filled.Place,
-        Icons.Outlined.Place,
+        designSystemR.drawable.feature_pantry_icon_filled,
+        designSystemR.drawable.feature_pantry_icon_outlined,
         PantryList::class
     ),
     RECIPES(
         recipesR.string.feature_recipes_title,
         recipesR.string.feature_recipes_title,
-        Icons.Rounded.Bookmark,
-        Icons.Outlined.BookmarkBorder,
+        designSystemR.drawable.feature_recipes_icon_filled,
+        designSystemR.drawable.feature_recipes_icon_outlined,
         RecipesList::class
     ),
     MEAL_PLANNER(
         mealsR.string.feature_meals_icon,
         mealsR.string.feature_meals_title,
-        Icons.Filled.Notifications,
-        Icons.Outlined.Notifications,
+        designSystemR.drawable.feature_meal_planner_icon_filled,
+        designSystemR.drawable.feature_meal_planner_icon_outlined,
         MealPlanner::class
     ),
 }
